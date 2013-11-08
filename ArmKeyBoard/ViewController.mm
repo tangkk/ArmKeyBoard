02d701e24e8d173750ed9096eea5e96375972d81
tangkk <dengjunqi06323011@gmail.com>
2013-11-08 17:49:39 +0800

    opencv support and convex hull contour detection

---
+++ ArmKeyBoard/ViewController.mm
@@ -0,0 +1,310 @@
+//
+//  ViewController.mm
+//  ArmKeyBoard
+//
+//  Created by tangkk on 26/10/13.
+//  Copyright (c) 2013 tangkk. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MobileCoreServices/UTCoreTypes.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#import "opencv2/opencv.hpp"
+
+//#define CANNY
+
+using namespace std;
+
+@interface ViewController () {
+    // Drawing Elements
+    UIBezierPath *path;
+    CGPoint PPoint;
+    CGPoint lastPoint;
+    CGFloat brush;
+    
+    // threshold
+    int thresh ;
+    int max_thresh;
+    // minimum area of a contour
+    int contourmin;
+    int max_contourmin;
+    // The contours and hulls
+    vector<vector<cv::Point> > mycontours;
+    vector<vector<cv::Point> > myhulls;
+}
+@property (strong, nonatomic) IBOutlet UIImageView *mainImage;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Initialize opencv variables
+    thresh = 100;
+    max_thresh = 255;
+    contourmin = 100;
+    max_contourmin = 100;
+    
+    // Initialize drawing variables
+    brush = 2;
+    [self.view addGestureRecognizer:[[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(clearImage)]];
+}
+
+- (cv::Mat)cvMatFromUIImage:(UIImage *)image
+{
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+    CGFloat cols = image.size.width;
+    CGFloat rows = image.size.height;
+    
+    cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels
+    
+    CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
+                                                    cols,                       // Width of bitmap
+                                                    rows,                       // Height of bitmap
+                                                    8,                          // Bits per component
+                                                    cvMat.step[0],              // Bytes per row
+                                                    colorSpace,                 // Colorspace
+                                                    kCGImageAlphaNoneSkipLast |
+                                                    kCGBitmapByteOrderDefault); // Bitmap info flags
+    
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+    CGContextRelease(contextRef);
+    CGColorSpaceRelease(colorSpace);
+    
+    return cvMat;
+}
+
+-(UIImage *)UIImageFromCVMat:(cv::Mat)cvMat
+{
+    NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize()*cvMat.total()];
+    CGColorSpaceRef colorSpace;
+    
+    if (cvMat.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    }
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+    
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(cvMat.cols,                                 //width
+                                        cvMat.rows,                                 //height
+                                        8,                                          //bits per component
+                                        8 * cvMat.elemSize(),                       //bits per pixel
+                                        cvMat.step[0],                            //bytesPerRow
+                                        colorSpace,                                 //colorspace
+                                        kCGImageAlphaNone|kCGBitmapByteOrderDefault,// bitmap info
+                                        provider,                                   //CGDataProviderRef
+                                        NULL,                                       //decode
+                                        false,                                      //should interpolate
+                                        kCGRenderingIntentDefault                   //intent
+                                        );
+    
+    
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    return finalImage;
+}
+
+-(void) doContourOperationTarget: (cv::Mat &)targtImg  Src:(cv::Mat &)srcImg Mix:(cv::Mat &)mixImg{
+    cv::Mat threshold_output;
+    cv::Mat canny_output;
+    // tangkk - Contours are a vector of vector of points
+    vector<vector<cv::Point> > contours;
+    
+    // tangkk - So Vec4i may indicates 4 entries in each elements of the vector?
+    vector<cv::Vec4i> hierarchy;
+    
+    /// Detect edges using gray scale
+    cv::threshold( targtImg, threshold_output, thresh, 255, cv::THRESH_BINARY );
+    cv::Canny( targtImg, canny_output, thresh, thresh*2, 3 );
+    
+    cv::RNG rng(12345);
+    
+    /*************************Find and draw each contour********************************/
+    /// tangkk - Draw contours only
+    // contours – Detected contours. Each contour is stored as a vector of points
+#ifdef CANNY
+    findContours( canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+    cv::Mat contourdrawing = cv::Mat::zeros( canny_output.size(), CV_8UC4 );
+#else
+    findContours( threshold_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+    cv::Mat contourdrawing = cv::Mat::zeros( threshold_output.size(), CV_8UC4 );
+#endif
+    
+    //discard the small contours
+    mycontours.clear();
+    for( int i = 0; i< contours.size(); i++) {
+        double area = contourArea(contours[i]);
+        if (area < contourmin)
+            contours[i].clear();
+        else
+            mycontours.push_back(contours[i]);
+    }
+    
+    for( int i = 0; i< mycontours.size(); i++ ) {
+        cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( contourdrawing, mycontours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+    }
+    
+    
+    for( int i = 0; i< mycontours.size(); i++ ) {
+        cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( contourdrawing, mycontours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+    }
+    
+    /*************************Find the convex hull object for each contour********************************/
+    myhulls.clear();
+    vector<vector<cv::Point> >hull( mycontours.size() );
+    for( size_t i = 0; i < mycontours.size(); i++ ) {
+        convexHull( cv::Mat(mycontours[i]), hull[i], false );
+        myhulls.push_back(hull[i]);
+    }
+    
+    /// Draw contours + hull results
+    cv::Mat drawing;
+#ifdef CANNY
+    drawing = cv::Mat::zeros( canny_output.size(), CV_8UC4 );
+#else
+    drawing = cv::Mat::zeros( threshold_output.size(), CV_8UC4 );
+#endif
+    for( size_t i = 0; i< mycontours.size(); i++ )
+    {
+        cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, hull, (int)i, color, 2, 8, vector<cv::Vec4i>(), 0, cv::Point() );
+    }
+    
+    for( size_t i = 0; i< mycontours.size(); i++ )
+    {
+        cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255)  );
+        drawContours( drawing, hull, (int)i, color, 2, 8, vector<cv::Vec4i>(), 0, cv::Point() );
+    }
+    
+    
+    /*************************Mix the source and the drawings********************************/
+    double alpha, beta;
+    alpha = 0.8;
+    beta = 2 - alpha;
+    cv::addWeighted(srcImg, alpha, drawing, beta, 0, mixImg);
+    //_mainImage.image = [self UIImageFromCVMat:mixImg];
+
+    
+}
+
+// Photo Picking methods
+- (BOOL) startMediaBrowserFromViewController: (UIViewController *)controller
+                               usingDelegate:(id <UIImagePickerControllerDelegate, UINavigationControllerDelegate>) delegate {
+    
+    if (([UIImagePickerController isSourceTypeAvailable:
+          UIImagePickerControllerSourceTypeSavedPhotosAlbum] == NO)
+         || (delegate == nil)
+         || (controller == nil)) {
+        return NO;
+    }
+    
+    UIImagePickerController *mediaUI = [[UIImagePickerController alloc] init];
+    mediaUI.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    mediaUI.mediaTypes = [[NSArray alloc] initWithObjects:(NSString *) kUTTypeImage, nil]; //Only Images are allowed
+    mediaUI.allowsEditing = NO;
+    mediaUI.delegate = delegate;
+    
+    [controller presentViewController:mediaUI animated:YES completion:nil];
+    return YES;
+}
+
+- (IBAction)showImageBrowser:(id)sender {
+    NSLog(@"Action Called");
+    [self startMediaBrowserFromViewController:self usingDelegate:self];
+}
+
+// Delegate method for the pick controller media browser
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"delegate called!");
+    
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    UIImage *selectedImage;
+    
+    // Handle the original image only
+    if (CFStringCompare((CFStringRef) mediaType, kUTTypeImage, 0) == kCFCompareEqualTo) {
+        selectedImage = (UIImage *) [info objectForKey:UIImagePickerControllerOriginalImage];
+        // Set the display content mode
+        //_mainImage.contentMode = UIViewContentModeScaleAspectFit;
+         _mainImage.contentMode = UIViewContentModeScaleAspectFill;
+        
+        // Do Image processing using opencv here;
+        _mainImage.image = [self ConvexHullProcessSrcImage:selectedImage];
+        //_mainImage.image = selectedImage;
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *) ConvexHullProcessSrcImage:(UIImage *) SrcImg {
+    cv::Mat src = [self cvMatFromUIImage:SrcImg];
+    cv::Mat src_gray;
+    cv::Mat mix;
+    
+    cv::cvtColor( src, src_gray, cv::COLOR_BGR2GRAY );
+    cv::blur( src_gray, src_gray, cv::Size(3,3) );
+    [self doContourOperationTarget:src_gray Src:src Mix:mix];
+    return [self UIImageFromCVMat:mix];
+}
+
+// Drawing methods
+static CGPoint midPoint(CGPoint p0, CGPoint p1) {
+    return CGPointMake((p0.x + p1.x)/2, (p0.y + p1.y)/2);
+}
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    lastPoint = [touch locationInView:self.view];
+    path = [UIBezierPath bezierPath];
+    [path moveToPoint:lastPoint];
+}
+
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint currentPoint = [touch locationInView:self.view];
+    
+    CGPoint middlePoint = midPoint(lastPoint, currentPoint);
+    [path addQuadCurveToPoint:middlePoint controlPoint:lastPoint];
+    
+    UIGraphicsBeginImageContext(self.view.frame.size);
+    [self.mainImage.image drawInRect:CGRectMake(0, 0, self.mainImage.frame.size.width, self.mainImage.frame.size.height)];
+    [[UIColor blackColor] setStroke];
+    [path setLineWidth:brush];
+    [path stroke];
+    CGContextAddPath(UIGraphicsGetCurrentContext(), path.CGPath);
+    self.mainImage.image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    lastPoint = currentPoint;
+}
+
+- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+- (void) clearImage {
+    path = [UIBezierPath bezierPath];
+    self.mainImage.image = nil;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
