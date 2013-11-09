892776adf6348897af4162a1198ebd40d6c03ad6
tangkk <dengjunqi06323011@gmail.com>
2013-11-09 12:54:44 +0800

    Pointpolygon test

--- ArmKeyBoard/ViewController.mm
+++ ArmKeyBoard/ViewController.mm
@@ -33,6 +33,11 @@ using namespace std;
     // The contours and hulls
     vector<vector<cv::Point> > mycontours;
     vector<vector<cv::Point> > myhulls;
+    
+    // Image : Screen Ratio
+    float widthRatio;
+    float heightRatio;
+    float distRatio;
 }
 @property (strong, nonatomic) IBOutlet UIImageView *mainImage;
 
@@ -48,8 +53,12 @@ using namespace std;
     // Initialize opencv variables
     thresh = 100;
     max_thresh = 255;
-    contourmin = 100;
-    max_contourmin = 100;
+    contourmin = 50;
+    max_contourmin = 2000;
+    
+    widthRatio = 1;
+    heightRatio = 1;
+    distRatio = 1;
     
     // Initialize drawing variables
     brush = 2;
@@ -117,58 +126,75 @@ using namespace std;
     return finalImage;
 }
 
+// FIXME: The smallest size of the hull should be consistent on the screen no matter how large of how small the image is
 -(void) doContourOperationTarget: (cv::Mat &)targtImg  Src:(cv::Mat &)srcImg Mix:(cv::Mat &)mixImg{
     cv::Mat threshold_output;
     cv::Mat canny_output;
+    cv::Mat contourdrawing;
     // tangkk - Contours are a vector of vector of points
     vector<vector<cv::Point> > contours;
     
     // tangkk - So Vec4i may indicates 4 entries in each elements of the vector?
     vector<cv::Vec4i> hierarchy;
     
-    /// Detect edges using gray scale
-    cv::threshold( targtImg, threshold_output, thresh, 255, cv::THRESH_BINARY );
-    cv::Canny( targtImg, canny_output, thresh, thresh*2, 3 );
+    
+    //make sure there's exactly 20 contours extracted from the source
+    thresh = 100;
+    do {
+        /*************************Find and draw each contour********************************/
+        contours.clear();
+        
+#ifdef CANNY
+        cv::Canny( targtImg, canny_output, thresh, thresh*2, 3 );
+        findContours( canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+        contourdrawing = cv::Mat::zeros( canny_output.size(), CV_8UC4 );
+#else
+        cv::threshold( targtImg, threshold_output, thresh, 255, cv::THRESH_BINARY );
+        findContours( threshold_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+        contourdrawing = cv::Mat::zeros( threshold_output.size(), CV_8UC4 );
+#endif
+        
+        //discard the small contours until there are 20 left
+        contourmin = 50;
+        do {
+            mycontours.clear();
+            for( int i = 0; i< contours.size(); i++) {
+                double area = contourArea(contours[i]);
+                if (area > contourmin)
+                    mycontours.push_back(contours[i]);
+            }
+            contourmin++;
+        } while (mycontours.size() > 20 && contourmin < max_contourmin);
+        if (mycontours.size() > 20) {
+            thresh-=10;
+        } else {
+            thresh+=10;
+        }
+        cout << "mycontours.size() = " << mycontours.size() << "\n";
+        cout << "thresh = " << thresh << "\n";
+        cout << "contourmin = " << contourmin << "\n";
+    } while ((mycontours.size() < 20 && thresh > 5) || (mycontours.size() > 20 && thresh < max_thresh));
+    cout << "final mycontours.size() = " << mycontours.size() << "\n";
+    cout << "final thresh = " << thresh << "\n";
+    cout << "final contourmin = " << contourmin << "\n";
     
     cv::RNG rng(12345);
-    
-    /*************************Find and draw each contour********************************/
-    /// tangkk - Draw contours only
-    // contours – Detected contours. Each contour is stored as a vector of points
-#ifdef CANNY
-    findContours( canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-    cv::Mat contourdrawing = cv::Mat::zeros( canny_output.size(), CV_8UC4 );
-#else
-    findContours( threshold_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-    cv::Mat contourdrawing = cv::Mat::zeros( threshold_output.size(), CV_8UC4 );
-#endif
-    
-    //discard the small contours
-    mycontours.clear();
-    for( int i = 0; i< contours.size(); i++) {
-        double area = contourArea(contours[i]);
-        if (area < contourmin)
-            contours[i].clear();
-        else
-            mycontours.push_back(contours[i]);
-    }
-    
     for( int i = 0; i< mycontours.size(); i++ ) {
         cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( contourdrawing, mycontours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+        cv::drawContours( contourdrawing, mycontours, i, color, 1, 8, hierarchy, 0, cv::Point() );
     }
     
     
     for( int i = 0; i< mycontours.size(); i++ ) {
         cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( contourdrawing, mycontours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+        cv::drawContours( contourdrawing, mycontours, i, color, 1, 8, hierarchy, 0, cv::Point() );
     }
     
     /*************************Find the convex hull object for each contour********************************/
     myhulls.clear();
     vector<vector<cv::Point> >hull( mycontours.size() );
     for( size_t i = 0; i < mycontours.size(); i++ ) {
-        convexHull( cv::Mat(mycontours[i]), hull[i], false );
+        cv::convexHull( cv::Mat(mycontours[i]), hull[i], false );
         myhulls.push_back(hull[i]);
     }
     
@@ -182,23 +208,21 @@ using namespace std;
     for( size_t i = 0; i< mycontours.size(); i++ )
     {
         cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( drawing, hull, (int)i, color, 2, 8, vector<cv::Vec4i>(), 0, cv::Point() );
+        cv::drawContours( drawing, myhulls, (int)i, color, 2, 8, vector<cv::Vec4i>(), 0, cv::Point() );
     }
     
     for( size_t i = 0; i< mycontours.size(); i++ )
     {
         cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255)  );
-        drawContours( drawing, hull, (int)i, color, 2, 8, vector<cv::Vec4i>(), 0, cv::Point() );
+        cv::drawContours( drawing, myhulls, (int)i, color, 2, 8, vector<cv::Vec4i>(), 0, cv::Point() );
     }
     
     
     /*************************Mix the source and the drawings********************************/
     double alpha, beta;
-    alpha = 0.8;
-    beta = 2 - alpha;
+    alpha = 0.5;
+    beta = 1 - alpha;
     cv::addWeighted(srcImg, alpha, drawing, beta, 0, mixImg);
-    //_mainImage.image = [self UIImageFromCVMat:mixImg];
-
     
 }
 
@@ -239,11 +263,21 @@ using namespace std;
     if (CFStringCompare((CFStringRef) mediaType, kUTTypeImage, 0) == kCFCompareEqualTo) {
         selectedImage = (UIImage *) [info objectForKey:UIImagePickerControllerOriginalImage];
         // Set the display content mode
-        //_mainImage.contentMode = UIViewContentModeScaleAspectFit;
-         _mainImage.contentMode = UIViewContentModeScaleAspectFill;
+        _mainImage.contentMode = UIViewContentModeScaleAspectFit;
+        //_mainImage.contentMode = UIViewContentModeScaleAspectFill;
+        
+        cout << "image size x = " << selectedImage.size.width << " y= " << selectedImage.size.height << "\n";
+        cout << "screen size x = " << self.view.frame.size.width << " y=" << self.view.frame.size.height << "\n";
+        widthRatio = selectedImage.size.width / self.view.frame.size.width;
+        heightRatio = selectedImage.size.height / self.view.frame.size.height;
+        distRatio = sqrt(widthRatio*widthRatio + heightRatio*heightRatio);
+        cout << "widthRatio = " << widthRatio  << "\n";
+        cout << "heightRatio = " << heightRatio << "\n";
+        cout << "distRatio = " << distRatio << "\n";
         
         // Do Image processing using opencv here;
         _mainImage.image = [self ConvexHullProcessSrcImage:selectedImage];
+        
         //_mainImage.image = selectedImage;
     }
     
@@ -262,34 +296,59 @@ using namespace std;
 }
 
 // Drawing methods
-static CGPoint midPoint(CGPoint p0, CGPoint p1) {
-    return CGPointMake((p0.x + p1.x)/2, (p0.y + p1.y)/2);
+//static CGPoint midPoint(CGPoint p0, CGPoint p1) {
+//    return CGPointMake((p0.x + p1.x)/2, (p0.y + p1.y)/2);
+//}
+
+- (void) checkInHullPosX:(int)x Y:(int)y {
+    bool isInHull = false;
+    float dist;
+    int scaleX, scaleY;
+    scaleX = x*widthRatio;
+    scaleY = y*heightRatio;
+    
+    //FIXME: Please fix it through scaling by the ratio between image size and screen size
+    for (int i = 0; i < myhulls.size(); i++) {
+        dist = (float)cv::pointPolygonTest( myhulls[i], cv::Point2f(scaleX,scaleY), true );
+        
+        // FIXME: what about the distance, should it also be scaled down?
+        if(dist > 0) {
+            dist /= distRatio; //Scale down the dist
+            cout << "The current pos is in Hull " << i << " with distance " << dist << "\n";
+            isInHull = true;
+        }
+    }
+    if (!isInHull) {
+        cout << "The current pos is outside " << "\n";
+    }
+    cout << "current pos x = " << x << " y = " << y << "\n";
 }
 
 - (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     lastPoint = [touch locationInView:self.view];
-    path = [UIBezierPath bezierPath];
-    [path moveToPoint:lastPoint];
+    [self checkInHullPosX:lastPoint.x Y:lastPoint.y];
+//    path = [UIBezierPath bezierPath];
+//    [path moveToPoint:lastPoint];
 }
 
 - (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [touches anyObject];
-    CGPoint currentPoint = [touch locationInView:self.view];
-    
-    CGPoint middlePoint = midPoint(lastPoint, currentPoint);
-    [path addQuadCurveToPoint:middlePoint controlPoint:lastPoint];
-    
-    UIGraphicsBeginImageContext(self.view.frame.size);
-    [self.mainImage.image drawInRect:CGRectMake(0, 0, self.mainImage.frame.size.width, self.mainImage.frame.size.height)];
-    [[UIColor blackColor] setStroke];
-    [path setLineWidth:brush];
-    [path stroke];
-    CGContextAddPath(UIGraphicsGetCurrentContext(), path.CGPath);
-    self.mainImage.image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    lastPoint = currentPoint;
+//    UITouch *touch = [touches anyObject];
+//    CGPoint currentPoint = [touch locationInView:self.view];
+//
+//    CGPoint middlePoint = midPoint(lastPoint, currentPoint);
+//    [path addQuadCurveToPoint:middlePoint controlPoint:lastPoint];
+//    
+//    UIGraphicsBeginImageContext(self.view.frame.size);
+//    [self.mainImage.image drawInRect:CGRectMake(0, 0, self.mainImage.frame.size.width, self.mainImage.frame.size.height)];
+//    [[UIColor blackColor] setStroke];
+//    [path setLineWidth:brush];
+//    [path stroke];
+//    CGContextAddPath(UIGraphicsGetCurrentContext(), path.CGPath);
+//    self.mainImage.image = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//    
+//    lastPoint = currentPoint;
 }
 
 - (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
